7f6596c0f051dcc254c7fde97693beda54085b31
buqento <buqento@gmail.com>
2022-10-27 17:27:26 +0700

    post detail and comment

--- components/Cards.js
+++ components/Cards.js
@@ -36,9 +36,13 @@ export const PostCard = ({ item }) => {
     const { title } = item
     return (
         <div class="flex justify-center">
-            <div class="block p-6 rounded-lg shadow-lg bg-white w-full">
-                <h5 class="text-gray-900 text-xl leading-tight mb-2">{title}</h5>
-                <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">More</button>
+            <div class="block p-6 rounded-lg shadow-lg bg-white w-full space-y-4">
+                <p class="font-light text-lg">
+                    {title}
+                </p>
+                <button type="button" className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+                    View Detail
+                </button>
             </div>
         </div>
     )
--- components/Posts.js
+++ components/Posts.js
@@ -1,16 +1,27 @@
 import Script from "next/script"
+import { useState } from "react"
 import useFetch from "../hooks/useFetch"
 import Albums from "./Albums"
-import { PostCard } from "./Cards"
 
 const Posts = ({ userId }) => {
 
     const [posts] = useFetch(`http://localhost:3001/posts?userId=${userId}`)
 
+    const [selected, setSelected] = useState()
+
     const renderPost = (item, index) => {
         return (
-            <div key={index}>
-                <PostCard item={item} />
+            <div class="flex justify-center" key={index}>
+                <div class="block p-6 rounded-lg shadow-lg bg-white w-full space-y-4">
+                    <p class="font-light text-lg line-clamp-3">
+                        {item.title}
+                    </p>
+                    <button type="button" className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom"
+                        onClick={() => setSelected(item)}
+                    >
+                        View Detail
+                    </button>
+                </div>
             </div>
         )
     }
@@ -18,7 +29,7 @@ const Posts = ({ userId }) => {
     const renderComment = (item, index) => {
         const { name, body } = item
         return (
-            <div key={index} className="border p-2">
+            <div key={index} className="p-2">
                 <div className="font-medium">{name}</div>
                 <div className="font-light">{body}</div>
             </div>
@@ -29,16 +40,19 @@ const Posts = ({ userId }) => {
         const { id, title, body } = item
         const [comments] = useFetch(`http://localhost:3001/comments?postId=${id}`)
         return (
-            <div>
+            <div className="space-y-4">
                 <h1 className="text-2xl capitalize">{title}</h1>
                 <div>
                     {body}
                 </div>
                 <div>
-                    <div>
-                        {`${comments?.length} komentar`}
+                    {
+                        comments?.length > 0 &&
+                        <span class="inline-block py-1.5 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-green-600 text-white rounded">{`${comments?.length} comment`}</span>
+                    }
+                    <div className="divide-y">
+                        {comments?.map(renderComment)}
                     </div>
-                    {comments?.map(renderComment)}
                 </div>
             </div>
         )
@@ -118,7 +132,18 @@ const Posts = ({ userId }) => {
                     </div>
                 </div>
             </div>
-
+            {
+                selected &&
+                <div class="offcanvas offcanvas-bottom fixed bottom-0 flex flex-col max-w-full bg-white invisible bg-clip-padding shadow-sm outline-none transition duration-300 ease-in-out text-gray-700 left-0 right-0 border-none h-1/3 max-h-full" tabindex="-1" id="offcanvasBottom" aria-labelledby="offcanvasBottomLabel">
+                    <div class="offcanvas-header flex items-center justify-between p-4">
+                        <h5 class="offcanvas-title mb-0 leading-normal font-semibold" id="offcanvasBottomLabel">Detail</h5>
+                        <button type="button" class="btn-close box-content w-4 h-4 p-2 -my-5 -mr-2 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body flex-grow p-4 overflow-y-auto small">
+                        <Post item={selected} />
+                    </div>
+                </div>
+            }
 
         </>
     )
